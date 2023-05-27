8827b39c33acf12827a0334248c5753ef1e263f6
VSaverskyi <vovikck15@gmail.com>
2023-05-27 09:51:38 +0200

    change ContactList to function component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,17 @@
 import ContactListItem from "components/ContactListItem/ContactListItem";
-import { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { ContactListWrapper } from "./ContactList.styled";
 
-class ContactList extends Component {
-    
-
-    render() {
-        const { contactsFilter, onDeleteBtnClick } = this.props;
-        return (
-            <ContactListWrapper>
-                {contactsFilter.length === 0 ? (<li>Contact don`t find</li>) : (contactsFilter
-                .map((item) => (
-                    <ContactListItem key={item.id} item={item} onDeleteBtn={onDeleteBtnClick}/>
-                )))}
-            </ContactListWrapper>
-        )
-    }
+const ContactList = ({contactsFilter, onDeleteBtnClick}) => {  
+    return (
+        <ContactListWrapper>
+            {contactsFilter.length === 0 ? (<li>Contact don`t find</li>) : (contactsFilter
+            .map((item) => (
+                <ContactListItem key={item.id} item={item} onDeleteBtn={onDeleteBtnClick}/>
+            )))}
+        </ContactListWrapper>
+    )
 }
 
 export default ContactList;
